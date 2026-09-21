64ec1f8f9d1ca6a91f4421c89efff8fd047218d0
Jonathan Winner Naya <jonathan.w.n2303@gmail.com>
2026-09-21 11:55:27 +0700

    Tugas 6: Augmentasi Objek Request & Response

--- src/models/todoModel.ts
+++ src/models/todoModel.ts
@@ -1,17 +1,28 @@
 import pool from '../config/db.js';
 
 export const TodoModel = {
-  getByUserId: async (userId: number) => {
-    const [rows] = await pool.query('SELECT * FROM todos WHERE user_id = ?', [userId]);
+  getByUserId: async (userId: number, limit: number, offset: number) => {
+    const [rows] = await pool.query(
+      'SELECT * FROM todos WHERE user_id = ? ORDER BY id DESC LIMIT ? OFFSET ?',
+      [userId, limit, offset]
+    );
     return rows;
+  },
+
+  countByUserId: async (userId: number) => {
+    const [rows]: any = await pool.query(
+      'SELECT COUNT(*) AS total FROM todos WHERE user_id = ?',
+      [userId]
+    );
+    return rows[0].total as number;
   },
 
   getById: async (id: number, userId: number) => {
     const [rows]: any = await pool.query(
-        'SELECT * FROM todos WHERE id = ? AND user_id = ?',
-        [id, userId]
+      'SELECT * FROM todos WHERE id = ? AND user_id = ?',
+      [id, userId]
     );
-    return rows[0]; 
+    return rows[0];
   },
 
   create: async (userId: number, task: string) => {
@@ -24,21 +35,19 @@ export const TodoModel = {
 
   // Update task atau status is_completed
   update: async (id: number, task: string, isCompleted: boolean, userId: number) => {
-      const [result]: any = await pool.query(
-          'UPDATE todos SET task = ?, is_completed = ? WHERE id = ? AND user_id = ?',
-          [task, isCompleted, id, userId]
-      );
-      return result.affectedRows;
+    const [result]: any = await pool.query(
+      'UPDATE todos SET task = ?, is_completed = ? WHERE id = ? AND user_id = ?',
+      [task, isCompleted, id, userId]
+    );
+    return result.affectedRows;
   },
 
   // Hapus todo berdasarkan id dan userId
   delete: async (id: number, userId: number) => {
-      const [result]: any = await pool.query(
-          'DELETE FROM todos WHERE id = ? AND user_id = ?',
-          [id, userId]
-      );
-      return result.affectedRows;
+    const [result]: any = await pool.query(
+      'DELETE FROM todos WHERE id = ? AND user_id = ?',
+      [id, userId]
+    );
+    return result.affectedRows;
   }
-
 };
-
